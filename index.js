1aca3433e258aff9e2f5181e2a8ce6c6f0b46305
Rachel Rondon <rachel.sun.rondon@gmail.com>
2019-12-02 20:56:06 -0500

    Duplicate Encoder solution

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-/* Duplicate Encoder */
+/* Duplicate Encoder
+  - The goal of this exercise is to convert a string to a new string where each character in the new string is "("
+  if that character appears only once in the original string, or ")" if that character appears more than once in the original string.
+  Ignore capitalization when determining if a character is a duplicate.
+ */
+
+
+ let duplicateString = (theString) => {
+
+  let newString = "";
+
+  let lowerCasedString = theString.toLowerCase();
+
+   for (let i = 0; i < lowerCasedString.length; i++) {
+     /* if the character appears only once */
+     if (lowerCasedString.indexOf(lowerCasedString[i]) !== lowerCasedString.lastIndexOf(lowerCasedString[i])) {
+       newString += ")";
+    /* if the character appears more than once */
+     } else {
+       newString += "("
+     }
+   }
+   return newString;
+ }
+
+ duplicateString("din");
+ duplicateString("recede");
+ duplicateString("Success");
+ duplicateString("(( @")

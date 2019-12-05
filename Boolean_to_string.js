daad386e2dad16226c1c14351eb71e67d66769eb
Rachel Rondon <rachel.sun.rondon@gmail.com>
2019-12-04 19:25:26 -0500

    Boolean to String solution

---
+++ Boolean_to_string.js
@@ -0,0 +1,13 @@
+/* The input is either true or false and the output is the string representation of the input*/
+
+let booleanToString = (bool) => {
+
+  if (bool === true) {
+    return "True";
+  } else if (bool === false) {
+    retun "False";
+  }
+
+}
+
+booleanToString(true);

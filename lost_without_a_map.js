767b54111b4b9e24e7dfe31a9358a924509e862f
Rachel Rondon <rachel.sun.rondon@gmail.com>
2019-12-05 20:37:21 -0500

    lost without a map solution

---
+++ lost_without_a_map.js
@@ -0,0 +1,13 @@
+/* given an array of integers - return a new array with each value doubled */
+
+let doubleInteger = (array) => {
+  let newArray = [];
+
+  array.forEach((item) => {
+    newArray.push(item * 2);
+  })
+
+  return newArray; 
+}
+
+doubleInteger([1,2,3]);

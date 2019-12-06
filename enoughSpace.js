4c6ca828452cafc7b3ad8eaa10de1b30853b492a
Rachel Rondon <rachel.sun.rondon@gmail.com>
2019-12-05 21:52:08 -0500

    will there be enough space solution

---
+++ enoughSpace.js
@@ -0,0 +1,14 @@
+/* write a function that returns if all of the passengers can fit on the bus */
+
+let enoughSpace = (cap, on, wait) => {
+
+  let passengers = on + wait;
+
+  if (passengers <= cap) {
+    return 0;
+  } else {
+    return - (cap - passengers);
+  }
+}
+
+enoughSpace(10, 5, 5)

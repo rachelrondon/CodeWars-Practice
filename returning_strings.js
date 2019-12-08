4a18c8eedadb31757d18a21bbeb3f9080d0bdce2
Rachel Rondon <rachel.sun.rondon@gmail.com>
2019-12-07 22:52:20 -0500

    returning string solution

---
+++ returning_strings.js
@@ -0,0 +1,8 @@
+/* make a function that will return a greeting statement that uses an input;
+your program should return "Hello, <name> how are you doing today?" */
+
+let greeting = (name) => {
+  return `Hello, ${name} how are you doing today?`;
+}
+
+greeting("Rachel")
